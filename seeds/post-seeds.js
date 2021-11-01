7599c68bb5bfb75aac89dbcc58305b63e1c5dfcb
Kiri <kirilynne@gmail.com>
2021-11-01 16:15:51 -0400

    add data to post-seeds.js

---
+++ seeds/post-seeds.js
@@ -0,0 +1,37 @@
+[
+    {
+        "title": "The best color",
+        "content": "I love the color red!",
+        "user_id": 1
+    },
+    {
+        "title": "The best color",
+        "content": "I love the color orange!",
+        "user_id": 2
+    },
+    {
+        "title": "The best color",
+        "content": "I love the color yellow!",
+        "user_id": 3
+    },
+    {
+        "title": "The best color",
+        "content": "I love the color green!",
+        "user_id": 4
+    },
+    {
+        "title": "The best color",
+        "content": "I love the color blue!",
+        "user_id": 5
+    },
+    {
+        "title": "The best color",
+        "content": "I love the color indigo!",
+        "user_id": 6
+    },
+    {
+        "title": "The best color",
+        "content": "I love the color violet!",
+        "user_id": 7
+    },
+]
