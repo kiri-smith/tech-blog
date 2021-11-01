070e4e611209f1fdb487ba6d281fbd85cd57d51b
Kiri <kirilynne@gmail.com>
2021-11-01 15:59:22 -0400

    update seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,26 +1,23 @@
-const seedCategories = require('./category-seeds');
-const seedProducts = require('./product-seeds');
-const seedTags = require('./tag-seeds');
-const seedProductTags = require('./product-tag-seeds');
+const commentSeeds = require('./comment-seeds');
+const postSeeds = require('./post-seeds');
+const userSeeds = require('./user-seeds');
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
 
-  await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+    await commentSeeds();
+    console.log('\n----- CATEGORIES SEEDED -----\n');
 
-  await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
+    await postSeeds();
+    console.log('\n----- PRODUCTS SEEDED -----\n');
 
-  await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+    await userSeeds();
+    console.log('\n----- TAGS SEEDED -----\n');
 
-  process.exit(0);
+    process.exit(0);
 };
 
 seedAll();
