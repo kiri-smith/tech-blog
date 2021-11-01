4339897b420256236ee042ab04286ac8dcddb048
Kiri <kirilynne@gmail.com>
2021-11-01 16:40:28 -0400

    update comment.js

---
+++ public/comment.js
@@ -0,0 +1,44 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const commentText = document.querySelector('#comment-text').value.trim();
+
+    if (commentText) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ commentText }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to add comment');
+        }
+    }
+};
+
+const removeCommentHandler = async (event) => {
+
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/comments/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to delete comment');
+        }
+    }
+};
+
+document
+    .querySelector('.add-comment-form')
+    .addEventListener('submit', commentFormHandler);
+
+document
+    .querySelector('.comment-form')
+    .addEventListener('submit', removeCommentHandler);
