92a07aeb965035ef055ac3942416842ae7972c42
Kiri <kirilynne@gmail.com>
2021-11-02 15:15:35 -0400

    update homeRoutes.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const apiRouter = require('./api');
 const router = express.Router();
+const homeRoutes = require('./homeRoutes');
 
+router.use('/', homeRoutes);
 router.use("/api", apiRouter)
 
 module.exports = router;
